5ab8be1b9dc31f4ed74852c755ce87e6d0adc9f4
SHCKY\ondre_000 <ondrejzatloukal01@gmail.com>
2018-01-05 17:43:20 -0800

    Initial commit

---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyPickup : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pickupPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the collider Other is tagged with "Player" then...
+        if (other.CompareTag("Player"))
+        {
+            // ...add pick up particles...
+            Instantiate(pickupPrefab, transform.position, Quaternion.identity);
+
+            // ...decrement the total amount of flies...
+            FlySpawner.totalFlies--;
+
+            // ...update the score...
+            ScoreCounter.score++;
+
+            Destroy(gameObject);
+        }
+    }
+
+}
